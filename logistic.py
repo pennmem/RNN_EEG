715ac65e5ce665ac99b1e5ed7efe57b6358482aa
tungphan87 <tungphan87@gmail.com>
2017-04-06 18:02:12 -0400

    update logistic_class.py

--- logistic.py
+++ logistic.py
@@ -3,54 +3,70 @@ import numpy as np
 import theano.tensor as T
 rng = np.random
 
+def softmax(x):
+    x = x - np.max(x)
+    output = np.exp(x)/(np.exp(x).sum())
+    return output
+
+def sample_y(p):
+    return np.random.multinomial(1,p,size = 1)
+
+def find_class(y):
+    return np.where(y > 0)
 
 # simulate data
-N = 400
-feats = 784
+N = 1000
+feats = 10
 n_class = 3
-D = (rng.randn(N,feats), rng.randint(size = N, low=0, high=3))
+X = rng.randn(N,feats)
+W = rng.randn(feats,n_class)*0.2
+b = np.array([-0.5,0,0.3])
+o = np.apply_along_axis(softmax,1,np.dot(X,W) + b)
+y = np.apply_along_axis(sample_y,1,o)
+y = np.argmax(y, axis = 1)
 
+D = (X,y)
+
+# build model
 x = T.dmatrix("x")
-y = T.dvector("y")
+y = T.dmatrix("y")
 w = theano.shared(rng.randn(feats,n_class), name = 'w')
 b = theano.shared(value = np.zeros((1,n_class)), name='b', broadcastable = (True,False))
 p_1 = T.nnet.softmax(T.dot(x,w)+b)
 prediction = T.argmax(p_1)
 #xent = -y * T.log(p_1+0.0000001) - (1-y) * T.log(1-p_1-0.0000000001)
-xent = -(y*T.log(p_1+0.00000001))
-
-cost = xent.mean() + 0.01*(w**2).sum()
+xent = -T.sum(y*T.log(p_1),1)
+cost = xent.mean()
 gw, gb = T.grad(cost, [w,b])
 
-train = theano.function(inputs = [x,y], outputs = [prediction, xent.mean()], updates = ((w, w-0.01*gw), (b, b-0.01*gb)))
+train = theano.function(inputs = [x,y], outputs = [prediction, xent.mean()], updates = ((w, w-0.1*gw), (b, b-0.1*gb)))
 predict = theano.function(inputs =[x], outputs = prediction)
-
-
 training_steps = 10000
 for i in range(training_steps):
     pred, err = train(D[0], D[1])
-    print 'Training error: {0:3}'.format(err)
+    if i%100 == 0:
+        print 'Training cost after {0:3} iterations is: {1:3}'.format(i,err)
 
 
 
-x = T.dvector("x")
-y = T.dscalar("y")
-w = theano.shared(rng.randn(feats), name = 'w')
-b = theano.shared(0.0, name='b')
-p_1 = 1/(1 + T.exp(-T.dot(x,w) - b))
-prediction = p_1 > 0.5
-xent = -y * T.log(p_1+0.0000001) - (1-y) * T.log(1-p_1-0.0000000001)
-cost = xent.mean() + 0.01*(w**2).sum()
-gw, gb = T.grad(cost, [w,b])
 
-train_sgd = theano.function(inputs = [x,y], outputs = [prediction, xent.mean()], updates = ((w, w-0.1*gw/feats), (b, b-0.1*gb/feats)))
-predict = theano.function(inputs =[x], outputs = prediction)
+import numpy as np
+
+a = np.random.random((2,2))
+b = np.random.random((2,2))
+c = np.tensordot(a,b,[[1],[1]])
 
 
-training_steps = 1000
-for i in range(training_steps):
-    err = 0
-    for j in range(N):
-        pred, err_sgd = train_sgd(D[0][j,:], D[1][j])
-        err = err + err_sgd
-    print 'Training error: {0:3}'.format(err/N)
+
+# using shared variable
+state = theano.shared(0)
+inc = T.iscalar('inc')
+accumulator = theano.function([inc], state, updates = [(state, state+inc)])
+
+fn_of_state = state*2 + inc
+foo = T.scalar(dtype =  state.dtype)
+skip_shared = theano.function([inc, foo], fn_of_state, givens = [(state, foo)])
+skip_shared(1,30)
+print state.get_value()
+
+
--- logistic_class.py
+++ logistic_class.py
@@ -1,20 +1,49 @@
 # Logistic Regression Class
-
+# mini-batch gradient descent MBGD
 
 import theano
 import theano.tensor as T
 import numpy as np
 
 class LogisticRegression(object):
-    def __init__(self, input, n_in, n_out):
-        self.W = theano.shared(value = np.zeros(n_in,n_out), dtype = theano.config.floatX, name = 'W', borrow = True)
-        self.b = theano.shared(value = np.zeros(n_out,), dtype = theano.config.floatX, name = 'b', borrow = True)
-        self.p_1 = 1/(1 + T.exp(-T.dot(self.x,self.W) - self.b))
-        self.y_pred = T.argmax(self.p_1, axis = 1)
-        self.params = [self.W, self.b]
+    def __init__(self, input, n_input, n_feature, n_class):
+        self.W = theano.shared(value = np.zeros((n_input,n_feature), dtype = theano.config.floatX), name = 'W', borrow = True)
+        self.b = theano.shared(value = np.zeros((1,n_class), dtype = theano.config.floatX), name = 'b', borrow = True)
         self.input = input
+        self.p_1 = T.nnet.softmax(T.dot(self.input,self.W) + self.b)
+        self.y_pred = T.argmax(self.p_1, axis = 1) # find argmax
+        self.params = [self.W, self.b]
 
-    def negative_log_likelihood(selfself,y):
-        return -T.mean(T.log(self.p_1))
+
+    def negative_log_likelihood(self,y):
+        # cost function
+        return -T.sum(y*T.log(self.p_1),1)
+
+
+    def errors(self,y):
+        return T.mean(T.neq(self.y_pred,y))
+
+
+
+
+x = T.dmatrix("x")
+y = T.dmatrix("y")
+classifier = LogisticRegression(input = x, n_input = 1000, n_feature = 10, n_class = 3)
+cost = classifier.negative_log_likelihood(y).mean()
+gw, gb = T.grad(cost, [classifier.W,classifier.b])
+learning_rate = 0.1
+updates = [(classifier.W, classifier.W-learning_rate*gw),(classifier.b, classifier.b-learning_rate*gb)]
+batch_size = 10
+index = 10
+
+
+train_model = theano.function(inputs = [index], outputs = cost, updates = updates,
+                              givens=[(x, train_set_x[index*batch_size : (index+1)*batch_size])
+                                  ,((y, train_set_y[index*batch_size : (index+1)*batch_size]))])
+
+
+
+
+
 
 
